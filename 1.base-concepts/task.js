ea5e3117c89491ed597c2e9c842a16383f0dd6c3
Дина Абрамова <nightfox87@mail.ru>
2022-09-26 19:54:45 +0300

    Corrections

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,15 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let D = b**2 - (4*a*c);
-  if (D === 0) {
+  let d = b**2 - (4*a*c);
+  if (d === 0) {
     arr = -b/(2*a);
   }
-  if (D > 0) {
+  if (d > 0) {
     arr = [
-      (-b + Math.sqrt(D))/(2*a),
-      (-b - Math.sqrt(D) )/(2*a)
+      (-b + Math.sqrt(d))/(2*a),
+      (-b - Math.sqrt(d) )/(2*a)
     ];
-  }
-  if (D < 0) {
-    arr = null;
   }
   return arr;
 }
