93da7327e7ce821bd5b98a066278ffdb8b5b9712
Дина Абрамова <nightfox87@mail.ru>
2022-09-26 15:53:23 +0300

    base-concepts first homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let arr = [];
+  let D = b**2 - (4*a*c);
+  if (D === 0) {
+    arr = -b/(2*a);
+  }
+  if (D > 0) {
+    arr = [
+      (-b + Math.sqrt(D))/(2*a),
+      (-b - Math.sqrt(D) )/(2*a)
+    ];
+  }
+  if (D < 0) {
+    arr = null;
+  }
+  return arr;
 }
