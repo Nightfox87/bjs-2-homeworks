2141addf0d6e0a98c058fb5b76ae31c8eaa63b2c
Дина Абрамова <nightfox87@mail.ru>
2022-10-09 19:57:27 +0300

    Homework 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length === arr2.length && arr1.every(element => arr2.includes(element))) {
+      result = true;
+    } else result = false;
+  }
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 == 0).map(element => element * 10);
+  return resultArr;
 }
