1b11f26b9d06e3d55acc2418bec5eaaf5ce3006a
Дина Абрамова <nightfox87@mail.ru>
2022-10-10 11:38:04 +0300

    Corrections

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,5 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length === arr2.length && arr1.every(element => arr2.includes(element))) {
-      result = true;
-    } else result = false;
-  }
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
   return result;
 }
 
