dfcdeb56f4cb717955d3f193d35f7b0cf6cc905e
Дина Абрамова <nightfox87@mail.ru>
2022-10-11 12:33:07 +0300

    Homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+let stud1 = new Student("Alex", "male", 27);
+let stud2 = new Student("Mary", "female", 29);
+let stud3 = new Student("Ann", "female", 28);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subjectName = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...mark) {
+  this.marks.push(...mark);
+}
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return average = sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subjectName;
+  delete this.marks;
+  this.excluded = reason;
+}
