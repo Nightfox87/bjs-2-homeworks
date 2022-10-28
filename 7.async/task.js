23ca57f4463cb7dca6720d236cabb3baee78bf1e
Дина Абрамова <nightfox87@mail.ru>
2022-10-29 01:24:15 +0300

    Homework Async

---
+++ 7.async/task.js
@@ -0,0 +1,83 @@
+class AlarmClock{
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId;
+    }
+    addClock(time, callback, id) {
+        if (id === undefined) {
+            throw new Error("Ошибка!");
+        }
+        if (this.alarmCollection.some((item) => item.id === id)) {
+            console.error("Ошибка!");
+        }
+        this.alarmCollection.push({id, time, callback});
+    }
+    removeClock(id) {
+        let index = this.alarmCollection.findIndex((item) => item.id === id);
+        if (index != -1) {
+            return this.alarmCollection.splice(index, 1)[0];
+        }
+        return null;
+    }
+    getCurrentFormattedTime() {
+        return new Date().toTimeString().slice(0, 5);
+    }
+    start() {
+        function checkClock(clock) {
+            if (this.getCurrentFormattedTime() === clock.time) {
+                clock.callback();
+            }
+        }
+        if (this.timerId === null) {
+            this.timerId = setInterval(() => {
+                this.alarmCollection.forEach(item => checkClock(item))
+            });
+        }
+    }
+    stop() {
+        if (this.timerId != null) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+    printAlarms() {
+        this.alarmCollection.forEach((item) => console.log(item.id, item.time));
+    }
+    clearAlarms() {
+        this.alarmCollection = [];
+    }
+
+}
+
+function testCase() {
+    let alarm = new AlarmClock();
+    let currentTime = alarm.getCurrentFormattedTime;
+
+    alarm.addClock(
+        currentTime,
+        () => console.log("Пора вставать"),
+        1);
+    
+    const addMinutes = (minutes) => {
+        let time = new Date();
+        return time.setTime(time.getTime() + minutes * 60000).slice(0,5);
+    }
+    alarm.addClock(
+        addMinutes(1),
+        () => {
+            console.log("Давай, вставай уже!");
+            alarm.removeClock(2);
+        },
+    2);
+    alarm.addClock(
+        addMinutes(2),
+        () => {
+            console.log("Вставай, а то проспишь!");
+            alarm.stop();
+            alarm.clearAlarms();
+            alarm.printAlarms();
+        },
+    3);
+    alarm.start();
+
+}
